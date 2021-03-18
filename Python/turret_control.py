2ac1bf04a72a7bae0cebce01865ded1769be16f0
unknown <mellam1@student.op.ac.nz>
2021-03-19 12:55:39 +1300

    find_target now returns x and y position of first target

--- Python/turret_control.py
+++ Python/turret_control.py
@@ -14,8 +14,6 @@ def find_target():
         print("failed to get frame")
 
     else: #succed in getting frame
-        print("got frame")
-
 
         #gray scale the frame for processing
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,19 +26,26 @@ def find_target():
             minSize=(30, 30),
         )
 
-        #Draw a rectangle around each target
+        #Draw a rectangle around each target    
         for(x, y, w, h) in targets:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)            
 
-        target = targets[0]        
-
+        #Get first target, if there is one
+        if targets.all():
+            target = targets[0]
+ 
         #Show the tragets
-        cv2.imshow("Faces!", frame)        
+        cv2.imshow("Faces!", frame)  
+
+        return(target[0], target[1])      
 
 def main():
     loop = True
     while loop:
-        find_target()
+
+        #Get the position of the target in screen space
+        point = find_target()
+        print(point)
 
         #close on esc press
         k = cv2.waitKey(1)
